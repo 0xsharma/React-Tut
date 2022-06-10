584013b73de89a82ff386dabaf14cfc1a0824519
Shivam Sharma <shivam691999@gmail.com>
2022-06-11 00:47:32 +0530

    Added material-ui button

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-
+import Button from '@mui/material/Button';
+import { purple } from '@mui/material/colors';
+import { styled } from '@mui/material/styles';
 //useState is used to make variable reactive. 
 
 
 // var str = 'World!'
 
 export default function TextBox() {
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: 'black',
+    },
+    display: str==='shraddha'? 'none':'inline',
+  }));
 
   const [str, setStr] = useState('World!')
   
@@ -20,7 +31,6 @@ export default function TextBox() {
   
 
   function handleChange(e) {
-
     setStr(e.target.value)
     
   }
@@ -29,6 +39,15 @@ export default function TextBox() {
     <div>TextBox :: {str} 
     <br/><br/>
     
+    <ColorButton variant="contained">Contained</ColorButton> <br/>
+    <br/>
+    <Button variant="contained"
+      style={{background:'black', color:'yellow'}}>
+        contained
+    </Button> 
+    <br/>
+
+    <br/>
     <input type="text" onChange={handleChange} />
     {/* <button onClick={handleChange}> Change </button> */}
     
